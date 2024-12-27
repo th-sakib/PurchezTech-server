7a1c21de0e2c2ba3cc956146f0f8f2fa879c8c70
th-sakib <mdthamidulhasansakib@gmail.com>
2024-12-28 00:08:41 +0600

    (feat) update & delete cart item - implement update and delete cart item functionality

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -20,6 +20,7 @@ const addToCart = asyncHandler(async (req, res) => {
     cart = new Cart({ userId, items: [] });
   }
 
+  // check if the product is in the cart
   const findCurrentProductIndex = cart.items.findIndex(
     (item) => item.productId.toString() === productId
   );
@@ -78,4 +79,82 @@ const fetchCart = asyncHandler(async (req, res) => {
     );
 });
 
-export { addToCart, fetchCart };
+const updateCartItem = asyncHandler(async (req, res) => {
+  const { productId, userId, quantity } = req.body;
+
+  if ((!productId, !userId, quantity <= 0)) {
+    throw new ApiError(404, "Invalid data provided");
+  }
+
+  const cart = await Cart.findOne({ userId });
+
+  if (!cart) {
+    throw new ApiError(404, "Cart not found");
+  }
+
+  const findCurrentProductIndex = cart.items.findIndex(
+    (item) => item.productId.toString() === productId
+  );
+
+  if (findCurrentProductIndex === -1) {
+    throw new ApiError(404, "Cart item not found");
+  }
+
+  if (cart.items[findCurrentProductIndex].quantity !== quantity) {
+    cart.items[findCurrentProductIndex].quantity = quantity;
+    await cart.save();
+  }
+
+  await cart.populate({
+    path: "items.productId",
+    select: "image title price salePrice",
+  });
+
+  const populateCartItems = cart.items.map((item) => ({
+    productId: item.productId ? item.productId._id : null,
+    image: item.productId ? item.productId.image : null,
+    price: item.productId ? item.productId.price : null,
+    salePrice: item.productId ? item.productId.salePrice : null,
+    quantity: item.quantity,
+  }));
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, { ...cart._doc, populateCartItems }));
+});
+
+const deleteCartItem = asyncHandler(async (req, res) => {
+  const { userId, productId } = req.params;
+
+  if (!userId || !productId) {
+    throw new ApiError(404, "Invalid data provided");
+  }
+
+  const cart = await Cart.findOne({ userId }).populate({
+    path: "items.productId",
+    select: "image title price salePrice",
+  });
+
+  const findCurrentProductIndex = cart.items.findIndex(
+    (item) => item.productId._id.toString() === productId
+  );
+
+  if (findCurrentProductIndex === -1) {
+    throw new ApiError(404, "Cart item not found");
+  }
+
+  const initialItemsCount = cart.items.length;
+  cart.items = cart.items.filter(
+    (item) => item.productId && item.productId._id.toString() !== productId
+  );
+
+  if (initialItemsCount > cart.items.length) {
+    await cart.save();
+  } else {
+    throw new ApiError(500, "cart item deletion failed");
+  }
+
+  res.status(200).json(new ApiResponse(200, { ...cart._doc }));
+});
+
+export { addToCart, fetchCart, updateCartItem, deleteCartItem };
--- src/routes/shop/cart.routes.js
+++ src/routes/shop/cart.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { addToCart, fetchCart } from "../../controllers/cart.controller.js";
+import {
+  addToCart,
+  deleteCartItem,
+  fetchCart,
+  updateCartItem,
+} from "../../controllers/cart.controller.js";
 
 const router = Router();
 
 router.post("/add-to-cart", addToCart);
 router.get("/get-cart/:userId", fetchCart);
+router.put("/update-cart", updateCartItem);
+router.delete("/delete-cart/:productId/:userId", deleteCartItem);
 
 export default router;
