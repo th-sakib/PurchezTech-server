5d95aec64e4eff5be67fa35af4429b350e5beae4
th-sakib <mdthamidulhasansakib@gmail.com>
2024-12-29 20:41:17 +0600

    fixes - cart route updating to patch method

--- src/routes/shop/cart.routes.js
+++ src/routes/shop/cart.routes.js
@@ -10,7 +10,7 @@ const router = Router();
 
 router.post("/add-to-cart", addToCart);
 router.get("/get-cart/:userId", fetchCart);
-router.put("/update-cart", updateCartItem);
+router.patch("/update-cart", updateCartItem);
 router.delete("/delete-cart/:productId/:userId", deleteCartItem);
 
 export default router;
